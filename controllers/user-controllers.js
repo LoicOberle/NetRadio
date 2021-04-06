650688db31e163f2288af7d27b87ab347b2e1786
GaetanCom <comacle.gaetan@gmail.com>
2021-04-06 15:16:28 +0200

    Page de l'announcer

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -45,11 +45,10 @@ exports.validRegister = async(req, res, next) => {
 exports.userLive = async (req, res, next) => {
     let username = req.params.username;
 
-    console.log(username);
-
-    res.render('containers/index', {
+    res.render('containers/announcerLive', {
         presentator: username, 
         liveTitle: 'Overwatch un dead game ?',
-        LivePresentation : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at dolor ac massa lobortis dignissim. Duis dictum tristique est non varius. Proin sodales aliquet felis, eu tempus dolor faucibus id. Vivamus vitae dui ut eros posuere gravida. Quisque arcu sem, luctus sit amet eros et, placerat iaculis tortor. Duis sed nunc nec justo posuere eleifend vel viverra turpis. Morbi arcu dolor, maximus euismod mi sit amet, accumsan congue est. Sed malesuada hendrerit magna, a pulvinar sem dapibus in. Pellentesque fermentum arcu id est posuere, sit amet consequat purus pulvinar. Donec non ligula tristique, ullamcorper enim non, vestibulum dui.'
+        LivePresentation : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at dolor ac massa lobortis dignissim. Duis dictum tristique est non varius. Proin sodales aliquet felis, eu tempus dolor faucibus id. Vivamus vitae dui ut eros posuere gravida. Quisque arcu sem, luctus sit amet eros et, placerat iaculis tortor. Duis sed nunc nec justo posuere eleifend vel viverra turpis. Morbi arcu dolor, maximus euismod mi sit amet, accumsan congue est. Sed malesuada hendrerit magna, a pulvinar sem dapibus in. Pellentesque fermentum arcu id est posuere, sit amet consequat purus pulvinar. Donec non ligula tristique, ullamcorper enim non, vestibulum dui.',
+        jsFile: "../../js"
     })
 }
