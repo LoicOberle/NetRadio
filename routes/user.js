6644e88f3c527259870e2d1fa75afec26c627754
Loic Oberle <loicoberle@neuf.fr>
2021-04-07 17:03:33 +0200

    modif route user

--- routes/user.js
+++ routes/user.js
@@ -5,5 +5,15 @@ const userControllers = require('../controllers/user-controllers');
 
 router.get('/:username/live', userControllers.userLive);
 
+router.get('/songList', (req, res) => {
+    console.log('giving soundList');
+    var files = fs.readdirSync('./public/sounds');
+    res.json(files)
+})
 
+router.get("/podcastList", (req, res) => {
+     console.log('giving podcast List');
+    var files = fs.readdirSync('./public/podcasts');
+    res.json(files)
+})
 module.exports = router;
