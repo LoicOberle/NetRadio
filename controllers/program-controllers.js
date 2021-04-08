db00c751f4ee9beabbfa948e626cd3ee3091a89d
keduma <kevin.dupont-marin@outlook.fr>
2021-04-08 21:25:04 +0200

    programs page sans avoi de données du controller

--- controllers/program-controllers.js
+++ controllers/program-controllers.js
@@ -4,5 +4,10 @@ const express  = require('express');
 DOMParser = require('xmldom').DOMParser;
 
 exports.index = async(req, res, next) => {
-    res.render('containers/programs')
+    let sql = "SELECT Broadcast.title, Slot.date_start, Slot.date_start FROM Slot INNER JOIN Broadcast ON Slot.id_broadcast = Broadcast.idBroadcast;"
+    let data = await bdd.query(sql)
+
+    res.render('containers/programs', {
+        tab_data: data
+    })
 }
